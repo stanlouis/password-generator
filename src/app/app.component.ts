3280e4690de2a549b434eca06c9dfd757b75a03c
Stanley Louis <stanlouis@msn.com>
2020-01-28 21:58:06 -0500

    An Angular password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghjklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) { validChars += letters; }
+    if (this.includeNumbers) { validChars += numbers; }
+    if (this.includeSymbols) { validChars += symbols; }
+
+    let generatedPassword = '';
+
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value, 10);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
 }
